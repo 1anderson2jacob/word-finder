d7337e21312215811a3a0dd63cb3ea68561d2190
jacob <1anderson2jacob@gmail.com>
2020-07-02 17:10:50 -0700

    modified config.js to reflect heroku's preferred postgreql connection method

--- config.js
+++ config.js
@@ -1,14 +1,25 @@
 'use strict';
 
 require('dotenv').config();
+// const { Pool } = require('pg');
 
-const { Pool } = require('pg');
-const isProduction = process.env.NODE_ENV === 'production';
-const connectionString =
-  `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+// const isProduction = process.env.NODE_ENV === 'production';
+// const connectionString =
+//   `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 
-const pool = new Pool({
-  connectionString: isProduction ? process.env.HEROKU_POSTGRESQL_BLACK_URL : connectionString, ssl: isProduction,
+// const pool = new Pool({
+//   connectionString: isProduction ? process.env.HEROKU_POSTGRESQL_BLACK_URL : connectionString, ssl: isProduction,
+// });
+
+// module.exports = { pool };
+
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
-module.exports = { pool };
+module.exports = { client };
