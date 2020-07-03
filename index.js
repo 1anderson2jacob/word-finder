adb6c0d961cbe75a8b89c9dc18eaf9a34fa90413
jacob <1anderson2jacob@gmail.com>
2020-07-03 11:46:20 -0700

    changed getWord to a promise, removed queryCreater() and instead made queryString

--- index.js
+++ index.js
@@ -11,27 +11,20 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-//req.body.
 const getWord = (req, res) => {
   console.log(req.query);
-  // pool.query('SELECT * FROM words WHERE word LIKE \'tw_l__\';', (err, results) => {
-  pool.query(queryCreator(req.query.incompleteWord), (err, results) => {
-    if (err) {
-      throw err;
-    }
-    res.status(200).json(results.rows);
-  });
+  let queryString = 'SELECT * FROM words WHERE word LIKE $1;'
+  pool
+    .query(queryString, [req.query.incompleteWord])
+    .then(results => {
+      res.status(200).json(results.rows);
+    })
+    .catch(err => console.error(err.stack))
 }
 
 app.route('/')
   .get(getWord);
 
-app.listen(process.env.PORT || 80, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('Server Listening');
 })
-
-function queryCreator(incompleteWord) {
-  let queryString = `SELECT * FROM words WHERE word LIKE \\'${incompleteWord}\\';`;
-
-  return queryString;
-}
